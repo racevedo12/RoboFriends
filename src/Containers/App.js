f8eaea593e0ba88372fea80b48cd7478895dfe78
racevedo12 <ronny.acv2@hotmail.com>
2022-01-10 18:50:07 -0500

    Replacing lifecycle methods with hooks such as useState and useEffect

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,62 +1,51 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import "./App.css";
 import Scroll from "../Components/Scroll";
 import ErrorBoundry from "../Components/ErrorBoundry";
-class App extends Component
-{
-    constructor()
-    {
-        super()
-        this.state = 
-        {
-            robots: [],
-            searchfield: "",
-        };
-    };
 
-    componentDidMount()
+const App = () =>
+{
+
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchField] = useState("");
+
+    useEffect( () =>
     {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(res => res.json() )
-            .then(users => this.setState( {robots: users} ) );
-    };
+            .then(users => setRobots( users ) );
+    }, []);
 
-    onSearchChange = (e) =>
+    const onSearchChange = (e) =>
     {
-        this.setState({searchfield: e.currentTarget.value});
+        setSearchField(e.currentTarget.value);
     };
 
-    render()
-    {
-        const {robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+        return (
+            robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        )
+    });
 
-        const filteredRobots = robots.filter( (robot) =>
-        {
-            return(
-                robot.name.toLowerCase().includes(searchfield.toLowerCase() )
-            )
-        });
+    return robots.length === 0 ?
+        <h1 className="tc"> Loading </h1>
 
-        return robots.length === 0 ? 
-            <h1 className="tc"> Loading </h1>
+        :
 
-            : 
+        <div className="tc">
+            <h1 className="f1"> RoboFriends </h1>
+            <SearchBox onSearchChange={onSearchChange} />
 
-            <div className="tc">
-                    <h1 className="f1"> RoboFriends </h1>
-                    <SearchBox onSearchChange={this.onSearchChange}  />
+            <Scroll>
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots} />
+                </ErrorBoundry>
+            </Scroll>
 
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                    
-            </div>
-        
-    };
+        </div>
+   
     
 };
 
